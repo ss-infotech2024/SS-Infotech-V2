8871b80229673d144b01e25ed2e0f7d3ac5680fe
SS Infotech <hr@ssinfotech.co>
2025-10-04 14:54:14 +0530

    Delete admin directory

--- admin/src/pages/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen text-gray-700">
-      <h1 className="text-3xl font-bold mb-2">404 - Page Not Found</h1>
-      <p>Oops! The page you’re looking for doesn’t exist.</p>
-    </div>
-  );
-}
